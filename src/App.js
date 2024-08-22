f36f02c678ecd9b76700724549bff0c02e9896cb
Marcelo <marcelo.san.car@hotmail.com>
2024-08-22 18:39:44 -0300

    bixs.com.br config

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
-import Home from './components/Home';
-import Setup from './components/Setup';
-import Manager from './components/Manager';
-import './styles/App.css';
-import { SessionProvider } from './hooks/SessionContext';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import Home from "./components/Home";
+import Setup from "./components/Setup";
+import Manager from "./components/Manager";
+import "./styles/App.css";
+import { SessionProvider } from "./hooks/SessionContext";
+import { ProfileProvider } from "./hooks/ProfileContext";
 
 function App() {
   return (
     <ChakraProvider>
-      <SessionProvider> {/* Ensure this wraps the Manager */}
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/setup" element={<Setup />} />
-            <Route path="/manager" element={<Manager />} />
-          </Routes>
-        </Router>
+      <SessionProvider>
+        <ProfileProvider>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/setup" element={<Setup />} />
+              <Route path="/manager" element={<Manager />} />
+            </Routes>
+          </Router>
+        </ProfileProvider>
       </SessionProvider>
     </ChakraProvider>
   );
 }
 
 export default App;
-
